f0431866b807d9faf94bf9d4630bb15b534906cc
Sharon <Sharon@DESKTOP-LLJGQV8>
2019-04-23 12:38:38 +0200

    Creo un almacenamiento para los algoritmos

---
+++ src/algoritmosdeordenación/AlgoritmosDeOrdenación.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosdeordenación;
+
+/**
+ *
+ * @author Sharon
+ */
+public class AlgoritmosDeOrdenación {
+    public void ordenacionAlgoritmos(int[] num) {
+ } 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
